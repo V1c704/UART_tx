41b98952d8ebd05416cac14c6d79707186b86984
Vic <vdorcaro@gmail.com>
2026-05-16 14:03:58 +0300

    Update top.sv

--- top.sv
+++ top.sv
@@ -8,46 +8,68 @@ module top(
         output logic      out_tx
     );
     
-    logic a;
+    logic        toggle_out;
+    logic [19:0] baud_count;
+    logic [3:0]  bit_count;
+    logic        crc_out;
+    logic [10:0] reg_out;
+    logic        mux_out;
+    logic        baud_tick;
+    logic        bit_10_reached;
+    logic        start_condition;
+    logic        a;
+    logic        toggle_signal;
+    logic        bit_counter_rst;
+    
+    assign baud_tick = (baud_count == 20'd5208);
+    assign bit_10_reached = (bit_count == 4'd10);
+    assign a = baud_tick & bit_10_reached;
+    assign start_condition = start & (~toggle_out);
+    assign toggle_signal = a | start_condition;
+    assign bit_counter_rst = a | rst;
+    assign out_tx = (~toggle_out) | mux_out;
     
     toggle_ff toggle_ff(
-        .toggle(),
+        .toggle(toggle_signal),
         .clk(clk),   
         .rst(rst),   
-        .out()    
+        .out(toggle_out)    
     );
     
     counter_baud_rate counter_baud_rate(
-        .en(),       
-        .clk(),      
-        .rst_async(),
-        .rst_sync(), 
-        .out()       
+        .en(toggle_out),       
+        .clk(clk),      
+        .rst_async(baud_tick),
+        .rst_sync(rst), 
+        .out(baud_count)       
     );
     
     counter_bit_select counter_bit_select(
-        .en(),  
-        .rst(), 
-        .clk(), 
-        .out()  
+        .en(baud_tick),  
+        .rst(bit_counter_rst), 
+        .clk(clk), 
+        .out(bit_count)  
     );
     
     crc_calc crc_calc(
-        .in(),
-        .out()
+        .in(data_in),
+        .out(crc_out)
     );
     
     reg0 reg0(
-        .data_in(),   
-        .load(),      
-        .clk(),       
-        .rst(),       
-        .data_out()   
+        .data_in({1'b1,crc_out,data_in,1'b0}),   
+        .load(start & (bit_count == 4'd0) & (baud_count == 20'd0)),      
+        .clk(clk),       
+        .rst(rst),       
+        .data_out(reg_out)   
     );
     
     mux mux(
-        .in(), 
-        .sel(),
-        .out() 
+        .in(reg_out), 
+        .sel(bit_count),
+        .out(mux_out) 
     );
+    
+    
+    
 endmodule
